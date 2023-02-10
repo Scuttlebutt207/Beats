977f92e728a1e96e3b6971d8d42f1ca98bea59b6
ScuttleButt207 <daniel.golightly21@icloud.com>
2023-02-10 14:49:19 -0500

    Added beat tracker Beat measure scrolls across screen for the beat

--- beatmaker.py
+++ beatmaker.py
@@ -15,6 +15,7 @@ white = (255,255,255)
 gray = (128,128,128)
 green = (0,255,0)
 gold = (212,175,55)
+blue = (0,255,255)
 
 
 #Create screen using dimensions used above, name the caption and label
@@ -32,9 +33,14 @@ instruments = 6
 boxes = []
 #-1 is not active, 1 is active
 clicked = [[-1 for _ in range(beats)] for _ in range(instruments)]
+bpm = 240
+playing = True
+active_length = 0
+active_beat = 0
+beat_changed = True
 
 #define function for draw grid
-def draw_grid(clicks):
+def draw_grid(clicks, beat):
     #start by making the left side menu. it is a rectangle so I have to call 4 arguments
     #Fifth one outside of the [], determines the width of the edges and make it a hollow object
     left_box = pygame.draw.rect(screen,gray, [0, 0, 200, HEIGHT - 200], 5)
@@ -85,6 +91,8 @@ def draw_grid(clicks):
             #for each rectangle, we are returning which beat it is, and the row, as well as
             #the rectangle itself to determine if it was clicked on.
             boxes.append((rect, (i, j)))
+
+        active = pygame.draw.rect(screen, blue, [beat * ((WIDTH-200)//beats) + 200, 0, ((WIDTH - 200)//beats), instruments * 100], 5, 3)
     return boxes
 
 
@@ -95,7 +103,7 @@ while run:
     #as long as run is true, we will execute this code at the fps(60)
     timer.tick(fps)
     screen.fill(black)
-    boxes = draw_grid(clicked)
+    boxes = draw_grid(clicked, active_beat)
 
     #event handling
     for event in pygame.event.get():
@@ -106,6 +114,21 @@ while run:
                 if boxes[i][0].collidepoint(event.pos):
                     coords = boxes[i][1]
                     clicked[coords[1]][coords[0]] *= -1
+    
+    #fps * 60(seconds) = 3600 // bpm
+    beat_length = 3600 // bpm
+
+    if playing:
+        if active_length < beat_length:
+            active_length += 1
+        else:
+            active_length = 0
+            if active_beat < beats - 1:
+                active_beat +=1
+                beat_changed = True
+            else:
+                active_beat = 0
+                beat_changed = True
 
     #Throws everything on screen        
     pygame.display.flip()
